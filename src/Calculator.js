3b962ce060c2ea642ca99eb93d28940a3b7b9aa1
paulohfev <phfernandesev@gmail.com>
2020-11-27 21:20:28 -0300

    Add base functionality to calculator

--- src/Calculator.js
+++ src/Calculator.js
@@ -2,7 +2,16 @@ import React, { useState } from 'react';
 import './Calculator.scss';
 
 const Calculator = () => {
-  const [result, setResult] = useState([0]);
+  const [result, setResult] = useState("");
+  // console.log(result, "| I was rendered");
+
+  const onButtonClick = event => {
+    setResult(result + event.target.value);
+  };
+
+  const calculate = () => {
+    setResult(eval(result));
+  };
 
   return (
     <div className="calculator">
@@ -11,16 +20,21 @@ const Calculator = () => {
       </div>
 
       <div className="buttons">
-        <button className="button number" onClick={() => setResult(1)}>1</button>
-        <button className="button number" onClick={() => setResult(2)}>2</button>
-        <button className="button number">3</button>
-        <button className="button number">4</button>
-        <button className="button number">5</button>
-        <button className="button number">6</button>
-        <button className="button number">7</button>
-        <button className="button number">8</button>
-        <button className="button number">9</button>
-        <button className="button clear" onClick={() => setResult(0)}>Clear</button>
+        <button className="button number" value="1" onClick={onButtonClick}>1</button>
+        <button className="button number" value="2" onClick={onButtonClick}>2</button>
+        <button className="button number" value="3" onClick={onButtonClick}>3</button>
+        <button className="button number" value="4" onClick={onButtonClick}>4</button>
+        <button className="button number" value="5" onClick={onButtonClick}>5</button>
+        <button className="button number" value="6" onClick={onButtonClick}>6</button>
+        <button className="button number" value="7" onClick={onButtonClick}>7</button>
+        <button className="button number" value="8" onClick={onButtonClick}>8</button>
+        <button className="button number" value="9" onClick={onButtonClick}>9</button>
+        <button className="button operator" value="+" onClick={onButtonClick}>+</button>
+        <button className="button operator" value="-" onClick={onButtonClick}>-</button>
+        <button className="button operator" value="*" onClick={onButtonClick}>*</button>
+        <button className="button operator" value="/" onClick={onButtonClick}>/</button>
+        <button className="button equals" value="=" onClick={calculate}>=</button>
+        <button className="button clear" onClick={() => setResult("")}>Clear</button>
       </div>
     </div>
   );
