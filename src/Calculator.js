143b5c21236461c88ce76ce6b167445808a47cab
paulohfev <phfernandesev@gmail.com>
2020-11-29 19:45:19 -0300

    Update UI and add backspace button

--- src/Calculator.js
+++ src/Calculator.js
@@ -3,15 +3,20 @@ import './Calculator.scss';
 import Button from './components/Button';
 
 const Calculator = () => {
-  const [result, setResult] = useState("");
+  const initialState = "";
+  const [result, setResult] = useState(initialState);
   // console.log(result, "| I was rendered");
 
-  const onButtonClick = event => {
+  const displayValue = event => {
     setResult(result + event.target.value);
   };
 
-  const calculate = () => {
-    setResult(eval(result));
+  const removeSingleValue = () => {
+    setResult(result.length - 1)
+  };
+
+  const calculateValue = () => {
+    if (result.length > 0) setResult(eval(result));
   };
 
   return (
@@ -20,22 +25,30 @@ const Calculator = () => {
         <h2>{result}</h2>
       </div>
 
-      <div className="buttons">
-        <Button type="number" value="1" onClick={onButtonClick} />
-        <Button type="number" value="2" onClick={onButtonClick} />
-        <Button type="number" value="3" onClick={onButtonClick} />
-        <Button type="number" value="4" onClick={onButtonClick} />
-        <Button type="number" value="5" onClick={onButtonClick} />
-        <Button type="number" value="6" onClick={onButtonClick} />
-        <Button type="number" value="7" onClick={onButtonClick} />
-        <Button type="number" value="8" onClick={onButtonClick} />
-        <Button type="number" value="9" onClick={onButtonClick} />
-        <Button type="number" value="+" onClick={onButtonClick} />
-        <Button type="number" value="-" onClick={onButtonClick} />
-        <Button type="number" value="*" onClick={onButtonClick} />
-        <Button type="number" value="/" onClick={onButtonClick} />
-        <Button type="equals" value="=" onClick={calculate} />
-        <Button type="clear" name="Clear" onClick={() => setResult("")} />
+      <div className="keypad">
+        <div className="button-group-top">
+          <Button type="small" name="->" onClick={() => removeSingleValue} />
+          <Button type="small" name="Clear" onClick={() => setResult(initialState)} />
+        </div>
+
+        <div className="button-group-middle">
+          <Button type="small" value="7" onClick={displayValue} />
+          <Button type="small" value="8" onClick={displayValue} />
+          <Button type="small" value="9" onClick={displayValue} />
+          <Button type="small" value="4" onClick={displayValue} />
+          <Button type="small" value="5" onClick={displayValue} />
+          <Button type="small" value="6" onClick={displayValue} />
+          <Button type="small" value="1" onClick={displayValue} />
+          <Button type="small" value="2" onClick={displayValue} />
+          <Button type="small" value="3" onClick={displayValue} />
+          <Button type="large" value="0" onClick={displayValue} />
+        </div>
+
+        <Button type="small" value="+" onClick={displayValue} />
+        <Button type="small" value="-" onClick={displayValue} />
+        <Button type="small" value="*" onClick={displayValue} />
+        <Button type="small" value="/" onClick={displayValue} />
+        <Button type="small" value="=" onClick={calculateValue} />
       </div>
     </div>
   );
