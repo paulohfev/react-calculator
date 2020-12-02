f319481a55eb003e854eb868993f3ad348e716ff
paulohfev <phfernandesev@gmail.com>
2020-12-01 21:59:56 -0300

    Add Keypad component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,48 +1,17 @@
 import React, { useState } from 'react';
 import './Calculator.scss';
-import Button from './Button';
 import ResultScreen from './ResultScreen';
+import Keypad from './Keypad';
 
 const Calculator = () => {
-  const initialState = "";
-  const [result, setResult] = useState(initialState);
+  const [result, setResult] = useState("");
   // console.log(result, "| I was rendered");
-
-  const displayValue = event => {
-    setResult(prevResult => prevResult + event.target.value);
-  };
-
-  const removeSingleValue = () => {
-    if (result.length > 0) setResult(prevResult => prevResult.slice(0, -1));
-  };
-
-  const calculateValue = () => {
-    if (result.length > 0) setResult(prevResult => eval(prevResult));
-  };
 
   return (
     <div className="calculator">
       <ResultScreen result={result} />
 
-      <div className="keypad">
-        <Button name="&#11157;" onClick={removeSingleValue} />
-        <Button name="Clear" onClick={() => setResult(initialState)} />        
-        <Button value="7" onClick={displayValue} />
-        <Button value="8" onClick={displayValue} />
-        <Button value="9" onClick={displayValue} />
-        <Button value="4" onClick={displayValue} />
-        <Button value="5" onClick={displayValue} />
-        <Button value="6" onClick={displayValue} />
-        <Button value="1" onClick={displayValue} />
-        <Button value="2" onClick={displayValue} />
-        <Button value="3" onClick={displayValue} />
-        <Button value="0" onClick={displayValue} />
-        <Button value="+" onClick={displayValue} />
-        <Button value="-" onClick={displayValue} />
-        <Button value="x" onClick={displayValue} />
-        <Button value="/" onClick={displayValue} />
-        <Button value="=" onClick={calculateValue} />
-      </div>
+      <Keypad result={result} setResult={setResult} />
     </div>
   );
 };
---
+++ src/components/Keypad.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Button from './Button';
+import './Keypad.scss';
+
+const Keypad = ({ result, setResult }) => {
+
+  const displayValue = event => {
+    setResult(prevResult => prevResult + event.target.value);
+  };
+
+  const removeSingleValue = () => {
+    if (result.length > 0) setResult(prevResult => prevResult.slice(0, -1));
+  };
+
+  const calculateValue = () => {
+    if (result.length > 0) setResult(prevResult => eval(prevResult));
+  };
+
+  const valueButtons = ['7', '8', '9', '4', '5', '6', '1', '2', '3', '0', '+', '-', 'x', '/'];
+  const valueButtonsList = valueButtons.map((value) => {
+    return <Button value={value} onClick={displayValue} />
+  });
+
+  return (
+    <div className="keypad">
+      <Button name="&#11157;" onClick={removeSingleValue} />
+      <Button name="Clear" onClick={() => setResult("")} />
+
+      {valueButtonsList}
+
+      <Button name="=" onClick={calculateValue} />
+    </div>
+  );
+};
+
+export default Keypad;
